9c18da7bda840a53e507c1dce1681cfc6f0699b5
Forest <767425412lin@gmail.com>
2024-06-14 17:34:08 +0800

    feat: class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import { Component } from 'react';
 import './App.css';
+
+class Greeting extends Component {
+    render() {
+        return <h1>Greeting, {this.props.name}!</h1>;
+    }
+}
 
 function App() {
     return (
@@ -6,6 +13,7 @@ function App() {
             <header className="App-header">
                 <p>hello world！！！</p>
             </header>
+            <Greeting name="Newton" />
         </div>
     );
 }
