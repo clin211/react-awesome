3c2d9b53918b2e42cf26cfdddfbe69e5f5a7f772
Forest <767425412lin@gmail.com>
2024-07-23 18:31:35 +0800

    feat: 受控组件与非受控组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import FileUploadForm from './components/FileUploadForm'
+import MixedForm from './components/MixedForm'
 import RegistrationForm from './components/RegistrationForm'
+import RegistrationFormUncontrolled from './components/RegistrationFormUncontrolled'
 
 function App () {
 
@@ -8,6 +11,10 @@ function App () {
         <h1 className="text-3xl font-semibold mb-6">Registration Form</h1>
         <RegistrationForm />
       </div>
+      <h1 className="text-3xl font-semibold mb-6">Registration Form Uncontrolled</h1>
+      <RegistrationFormUncontrolled />
+      <MixedForm />
+      <FileUploadForm />
     </div>
   )
 }
