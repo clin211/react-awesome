7c12ba3679e304896c420d5028aee4d6e7d6bb4f
Forest <767425412lin@gmail.com>
2024-07-31 10:18:34 +0800

    feat: 配置 require 的支持

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import requireTransform from 'vite-plugin-require-transform'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    requireTransform({
+      fileRegex: /.js$|.jsx$/,
+    }),
+  ],
 })
