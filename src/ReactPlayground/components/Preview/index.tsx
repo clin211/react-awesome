c40f75c1a15a383e9cc012d24d289012d824fac8
Forest <767425412lin@gmail.com>
2024-08-14 16:51:04 +0800

    feat: 添加错误信息的提示功能

--- src/ReactPlayground/components/Preview/index.tsx
+++ src/ReactPlayground/components/Preview/index.tsx
@@ -3,12 +3,21 @@ import { PlaygroundContext } from "../../PlaygroundContext"
 import { compile } from "./compiler";
 import iframeRaw from './iframe.html?raw'
 import { IMPORT_MAP_FILE_NAME } from "../../files";
+import { Message } from '../Message';
+
+interface MessageData {
+    data: {
+        type: string
+        message: string
+    }
+}
 
 export default function Preview() {
 
     const { files } = useContext(PlaygroundContext)
     const [compiledCode, setCompiledCode] = useState('')
     const [iframeUrl, setIframeUrl] = useState(getIframeUrl());
+    const [error, setError] = useState('')
 
     useEffect(() => {
         const res = compile(files);
@@ -27,11 +36,22 @@ export default function Preview() {
         return URL.createObjectURL(new Blob([res], { type: 'text/html' }))
     }
 
+    const handleMessage = (msg: MessageData) => {
+        const { type, message } = msg.data
+        if (type === 'ERROR') setError(message)
+    }
+
     useEffect(() => {
         setIframeUrl(getIframeUrl())
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [files[IMPORT_MAP_FILE_NAME].value, compiledCode]);
 
+    useEffect(() => {
+        window.addEventListener('message', handleMessage)
+        return () => {
+            window.removeEventListener('message', handleMessage)
+        }
+    }, [])
     return <div style={{ height: '100%' }}>
         <iframe
             src={iframeUrl}
@@ -43,6 +63,7 @@ export default function Preview() {
                 border: 'none',
             }}
         />
+        <Message type='error' content={error} />
         {/* <Editor file={{
             name: 'dist.js',
             value: compiledCode,
